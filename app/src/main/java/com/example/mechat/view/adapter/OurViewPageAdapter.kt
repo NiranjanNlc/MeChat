911d937b48a6dcdae231ca5c08336dbaf508e9de
niranjannlc <niranjannlc10@gmail.com>
2021-10-14 10:03:12 +0545

    viewpGae adapter configured

---
+++ app/src/main/java/com/example/mechat/view/adapter/OurViewPageAdapter.kt
@@ -0,0 +1,25 @@
+package com.example.mechat.view.adapter
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import com.example.mechat.view.fragment.ChatFragment
+
+class OurViewPageAdapter(fm : FragmentManager, val behavior: Int) : FragmentPagerAdapter (fm,behavior)
+{
+    override fun getCount(): Int {
+       return behavior
+    }
+
+    override fun getItem(position: Int): Fragment
+    {
+        when (position) {
+            0 -> {
+                //  val homeFragment: HomeFragment = HomeFragment()
+                return ChatFragment()
+            }
+
+            else -> return ChatFragment()
+        }
+    }
+}
