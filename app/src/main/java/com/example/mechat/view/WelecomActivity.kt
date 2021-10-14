84df7ee3ecf17cc7d4f4c063da7109a56200e3e3
niranjannlc <niranjannlc10@gmail.com>
2021-10-14 10:30:59 +0545

    tablayout completed

---
+++ app/src/main/java/com/example/mechat/view/WelecomActivity.kt
@@ -0,0 +1,44 @@
+package com.example.mechat.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.viewpager.widget.PagerAdapter
+import com.example.mechat.databinding.MainScreenBinding
+import com.example.mechat.view.adapter.OurViewPageAdapter
+import com.google.android.material.tabs.TabLayout
+
+class WelecomActivity : AppCompatActivity()
+{
+    private lateinit var binding1 : MainScreenBinding
+    private lateinit var adapter: PagerAdapter
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding1.root)
+        val fragmentManager1 = supportFragmentManager
+
+        adapter =  OurViewPageAdapter(fragmentManager1,1)
+        binding1.viewPager.adapter=adapter
+        binding1.viewPager.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(binding1.tabView))
+        binding1.tabView.addOnTabSelectedListener(object: TabLayout.OnTabSelectedListener{
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                if (tab != null) {
+                    binding1.viewPager.currentItem = tab.position
+                }
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?)
+            {
+
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?)
+            {
+            }
+
+        })
+
+    }
+
+
+}
+}
