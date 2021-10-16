d3dc42002fa4f3cb15dfbc64b8332a6ba3681799
niranjannlc <niranjannlc10@gmail.com>
2021-10-16 17:25:02 +0545

    understanding the use of comparator operator and diffutil callback

--- app/src/main/java/com/example/mechat/modal/data/Users.kt
+++ app/src/main/java/com/example/mechat/modal/data/Users.kt
@@ -1,5 +1,6 @@
 package com.example.mechat.modal.data
 
-class Users(profilePic:String ,userName:String,mail:String,password : String,lastMessage:String
-            ,userId:String )    {
+class Users(val profilePic:String ,userName:String,mail:String,password : String,lastMessage:String
+            ,userId:String )
+{
 }
--- app/src/main/java/com/example/mechat/view/adapter/UserAdapter.kt
+++ app/src/main/java/com/example/mechat/view/adapter/UserAdapter.kt
@@ -1,61 +1,66 @@
 package com.example.mechat.view.adapter
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import android.widget.ListAdapter
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.DiffUtil.ItemCallback
 import androidx.recyclerview.widget.RecyclerView
+import com.example.mechat.databinding.UserItemsBinding
 import com.example.mechat.modal.data.Users
+import com.example.mechat.view.adapter.UserAdapter.*
 
 class UserAdapter constructor(val context : Context, val itemClickListener: ItemClickListener):
-    ListAdapter<Users, ReciepeListAdapter.RecipeListViewHolder>(MEAL_COMPARATOR)
+    ListAdapter<Users, UserListViewHolder>(COMPARATOR)
 {
 
     interface ItemClickListener{
         fun onItemClick(position: String)
     }
 
-    inner  class RecipeListViewHolder(var items: ): RecyclerView.ViewHolder(items.root)
-    {
+    inner  class UserListViewHolder(var items: UserItemsBinding): RecyclerView.ViewHolder(items.root) {
         init {
-            items.root.setOnClickListener{
-                items.items?.idMeal?.let { it1 -> itemClickListener.onItemClick(it1) }
+            items.root.setOnClickListener {
+
+
             }
+            }
+
+        fun bind(userItems: Users) {
+            items.userChats= userItems
         }
-        fun bind(mealItem: MealItem)
+
+    }
+
+     companion object COMPARATOR : DiffUtil.ItemCallback<Users>()
+    {
+        override fun areItemsTheSame(oldItem: Users, newItem: Users): Boolean
         {
-            items.items = mealItem
-
+            return oldItem.profilePic == newItem.profilePic
         }
 
-    }
-
-    companion object {
-        val MEAL_COMPARATOR = object : DiffUtil.ItemCallback<MealItem>() {
-            override fun areItemsTheSame(oldItem: MealItem, newItem: MealItem): Boolean {
-                println(" item same ")
-                return oldItem == newItem;
-            }
-
-            override fun areContentsTheSame(oldItem: MealItem, newItem: MealItem): Boolean {
-                print(" Content same ")
-                return oldItem.idMeal.equals(newItem.idMeal)
-            }
+        override fun areContentsTheSame(oldItem: Users, newItem: Users): Boolean {
+             return oldItem.equals(newItem)
         }
-
     }
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecipeListViewHolder
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserListViewHolder
     {
         println("On view create ")
         val inflater = LayoutInflater.from(parent.context)
-        val binding = ItemRecipeBinding.inflate(inflater)
-        return RecipeListViewHolder(binding)
+        val binding = UserItemsBinding.inflate(inflater)
+        return UserListViewHolder(binding)
 
     }
-    override fun onBindViewHolder(holder: RecipeListViewHolder, position: Int)
+    override fun onBindViewHolder(holder: UserListViewHolder, position: Int)
     {
-        val mealItem = getItem(position)
-        println( " see thid " + mealItem.strMeal)
-        holder.bind(mealItem)
-        Glide.with(context).load(mealItem.strMealThumb).into(holder.items.browserCellImage)
+        val userItems = getItem(position)
+        println( " see thid " + userItems.strMeal)
+        holder.bind(userItems)
+        Glide.with(context).load(userItems.strMealThumb).into(holder.items.browserCellImage)
         holder.items.executePendingBindings()
-    } {
+    }
+
 }
