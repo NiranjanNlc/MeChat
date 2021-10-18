72053a799695b136ffedf5ec758cee4cce76aa6a
niranjannlc <niranjannlc10@gmail.com>
2021-10-18 15:13:01 +0545

    sighnup activuty created

---
+++ app/src/main/java/com/example/mechat/view/SighUpActivity.kt
@@ -0,0 +1,11 @@
+package com.example.mechat.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SighUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sigh_up)
+    }
+}
