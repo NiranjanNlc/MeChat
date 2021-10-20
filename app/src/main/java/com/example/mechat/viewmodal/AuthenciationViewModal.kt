c6c4ccf844542699ecbfa505c5993f84e29c48cf
niranjannlc <niranjannlc10@gmail.com>
2021-10-20 11:24:56 +0545

    two way viewbinding not working

--- app/src/main/java/com/example/mechat/viewmodal/AuthenciationViewModal.kt
+++ app/src/main/java/com/example/mechat/viewmodal/AuthenciationViewModal.kt
@@ -1,5 +1,6 @@
 package com.example.mechat.viewmodal
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -9,16 +10,18 @@ import com.example.mechat.modal.data.Users
 class AuthenciationViewModal: ViewModel()
 {
     lateinit var user :  Users
-    var mail= MutableLiveData<String>()
-    var userName = MutableLiveData<String>()
-    var password = MutableLiveData<String>()
+    var mail= MutableLiveData<String>("niranjannlc10@gmail.com")
+    var userName = MutableLiveData<String>(" NirnjN")
+    var password = MutableLiveData<String>("123456")
 
     init{
 
     }
     fun sighnUp(): Boolean?
     {
+
         user= Users(userName =userName.value.toString(),mail = mail.value.toString(),password =password.value.toString() )
+        Log.i("user ",user.toString())
        return FireBaseService.sighnUpUser(user)
     }
 }
