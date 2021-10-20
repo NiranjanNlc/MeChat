236babe7eadd0a9f5130c55f44c63eecf47cc5a7
niranjannlc <niranjannlc10@gmail.com>
2021-10-20 07:43:40 +0545

    firebase database refrence

--- app/src/main/java/com/example/mechat/utils/FireBaseUtils.kt
+++ app/src/main/java/com/example/mechat/utils/FireBaseUtils.kt
@@ -2,9 +2,12 @@ package com.example.mechat.utils
 
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 
 object FirebaseUtils {
     val firebaseAuth: FirebaseAuth = FirebaseAuth.getInstance()
     val firebaseUser: FirebaseUser? = firebaseAuth.currentUser
+     val database = Firebase.database.reference
 }
