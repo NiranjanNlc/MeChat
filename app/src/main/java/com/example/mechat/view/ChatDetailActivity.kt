b6bb5dab048da9c30d6e72c9dd355edd79447932
niranjannlc <niranjannlc10@gmail.com>
2021-10-26 12:56:42 +0545

    adding chat detail activity

---
+++ app/src/main/java/com/example/mechat/view/ChatDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mechat.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mechat.R
+
+class ChatDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat_detail)
+    }
+}
