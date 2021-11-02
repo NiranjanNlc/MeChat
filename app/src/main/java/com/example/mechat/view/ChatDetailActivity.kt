ba14a82fb3c6ca4ae31cd1d6c6397a22621647ca
niranjannlc <niranjannlc10@gmail.com>
2021-11-02 16:24:19 +0800

    receiing the intent parameter in chat detail

--- app/src/main/java/com/example/mechat/view/ChatDetailActivity.kt
+++ app/src/main/java/com/example/mechat/view/ChatDetailActivity.kt
@@ -3,10 +3,14 @@ package com.example.mechat.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.mechat.R
+import com.example.mechat.utils.FirebaseUtils
 
 class ChatDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat_detail)
+         var senderId = FirebaseUtils.firebaseUser?.uid
+         var recieverId =intent.getStringExtra("userId")
+         var userName = intent.getStringExtra("username")
     }
 }
