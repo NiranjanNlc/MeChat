463023ced478b1d47ba2a41fbcf0de3e6793094a
niranjannlc <niranjannlc10@gmail.com>
2021-11-03 16:51:06 +0800

    adapter of detail chat sketched

--- app/src/main/java/com/example/mechat/view/adapter/DetailChatAdapter.kt
+++ app/src/main/java/com/example/mechat/view/adapter/DetailChatAdapter.kt
@@ -1,9 +1,12 @@
 package com.example.mechat.view.adapter
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.mechat.databinding.SampleRecieverBinding
+import com.example.mechat.databinding.SampleSenderBinding
 import com.example.mechat.modal.Message
 import com.example.mechat.utils.FirebaseUtils
 
@@ -12,15 +15,34 @@ class DetailChatAdapter(context: Context , var messages :List<Message>):
 {
     val recieverViewType : Int = 2;
     val senderViewType : Int = 1;
-    class ReceiverViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+    class ReceiverViewHolder(var binding: SampleRecieverBinding) : RecyclerView.ViewHolder(binding.root)
     {}
-    class SenderViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+    class SenderViewHolder(var binding: SampleSenderBinding) : RecyclerView.ViewHolder(binding.root)
     {}
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        TODO("Not yet implemented")
+        if(viewType ==2)
+        {
+            val inflater = LayoutInflater.from(parent.context)
+            val binding = SampleRecieverBinding.inflate(inflater)
+            return ReceiverViewHolder(binding);
+        }
+        else
+        {
+            val inflater = LayoutInflater.from(parent.context)
+            val binding = SampleSenderBinding.inflate(inflater)
+            return SenderViewHolder(binding)
+        }
     }
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int)
+    {
+         if(holder.javaClass == SenderViewHolder::class.java)
+         {
+            //code for binding
+         }
+        else
+         {
+            //code for binding
+         }
     }
     override fun getItemCount(): Int {
         TODO("Not yet implemented")
