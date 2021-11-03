cc2e9e15f3bd6d9bb06b054560da126b4f7b1bae
niranjannlc <niranjannlc10@gmail.com>
2021-11-03 16:12:54 +0800

    sepreate view holder for reciever nad sender

--- app/src/main/java/com/example/mechat/view/adapter/DetailChatAdapter.kt
+++ app/src/main/java/com/example/mechat/view/adapter/DetailChatAdapter.kt
@@ -1,6 +1,17 @@
 package com.example.mechat.view.adapter
 
-class DetailChatAdapter
+import android.content.Context
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.example.mechat.modal.Message
+
+class DetailChatAdapter(context: Context , messages :List<Message>)
 {
+    val recieverViewType : Int = 2;
+    val senderViewType : Int = 1;
+    inner class ReceiverViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+    {}
+    inner class SenderViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+    {}
 
 }
