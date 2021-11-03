4a0d5629a384b9d7a786ef2a22d921cb870288f7
niranjannlc <niranjannlc10@gmail.com>
2021-11-03 16:22:16 +0800

    skeletpn for two view holder

--- app/src/main/java/com/example/mechat/view/adapter/DetailChatAdapter.kt
+++ app/src/main/java/com/example/mechat/view/adapter/DetailChatAdapter.kt
@@ -2,16 +2,29 @@ package com.example.mechat.view.adapter
 
 import android.content.Context
 import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.mechat.modal.Message
 
-class DetailChatAdapter(context: Context , messages :List<Message>)
+class DetailChatAdapter(context: Context , messages :List<Message>):
+    RecyclerView.Adapter<RecyclerView.ViewHolder>()
 {
     val recieverViewType : Int = 2;
     val senderViewType : Int = 1;
-    inner class ReceiverViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+    class ReceiverViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
     {}
-    inner class SenderViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+    class SenderViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
     {}
-
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        TODO("Not yet implemented")
+    }
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+    override fun getItemViewType(position: Int): Int {
+        return super.getItemViewType(position)
+    }
 }
