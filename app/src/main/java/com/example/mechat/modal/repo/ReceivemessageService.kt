f8b3c45265fc408c92886ec1e0d7d1c8fd080be9
niranjannlc <niranjannlc10@gmail.com>
2021-11-28 16:33:03 +0800

    reciever section noot impleemnted

--- app/src/main/java/com/example/mechat/modal/repo/ReceivemessageService.kt
+++ app/src/main/java/com/example/mechat/modal/repo/ReceivemessageService.kt
@@ -1,6 +1,59 @@
 package com.example.mechat.modal.repo
 
+import android.content.ContentValues.TAG
+import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
+
 object ReceivemessageService
 {
+    private fun listenForMessages() {
+
+        val fromId = FirebaseAuth.getInstance().uid ?: return
+        val toId = toUser.uid
+        val ref = FirebaseDatabase.getInstance().getReference("/user-messages/$fromId/$toId")
+
+        ref.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onCancelled(databaseError: DatabaseError) {
+                Log.d(TAG, "database error: " + databaseError.message)
+            }
+
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                Log.d(TAG, "has children: " + dataSnapshot.hasChildren())
+                if (!dataSnapshot.hasChildren()) {
+
+                }
+            }
+        })
+
+        ref.addChildEventListener(object : ChildEventListener {
+            override fun onCancelled(databaseError: DatabaseError) {
+            }
+
+            override fun onChildMoved(dataSnapshot: DataSnapshot, previousChildName: String?) {
+            }
+
+            override fun onChildChanged(dataSnapshot: DataSnapshot, previousChildName: String?) {
+            }
+
+            override fun onChildAdded(dataSnapshot: DataSnapshot, previousChildName: String?) {
+                dataSnapshot.getValue(ChatMessage::class.java)?.let {
+                    if (it.fromId == FirebaseAuth.getInstance().uid) {
+                        val currentUser = LatestMessagesActivity.currentUser ?: return
+                        adapter.add(ChatFromItem(it.text, currentUser, it.timestamp))
+                    } else {
+                        adapter.add(ChatToItem(it.text, toUser, it.timestamp))
+                    }
+                }
+                recyclerview_chat_log.scrollToPosition(adapter.itemCount - 1)
+
+            }
+
+            override fun onChildRemoved(dataSnapshot: DataSnapshot) {
+            }
+
+        })
+
+    }
 
 }
