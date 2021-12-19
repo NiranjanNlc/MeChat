5e9772cc6739debddb9427eda48f79d0806d69c0
niranjannlc <niranjannlc10@gmail.com>
2021-12-19 20:12:16 +1200

    testing unsucessful for the firebase

--- app/src/test/java/com/example/mechat/modal/repo/AuthenciationServiceTest.kt
+++ app/src/test/java/com/example/mechat/modal/repo/AuthenciationServiceTest.kt
@@ -1,21 +1,24 @@
 package com.example.mechat.modal.repo
 
+import com.example.mechat.modal.data.Users
+import com.example.mechat.utils.FirebaseUtils
 import kotlinx.coroutines.*
+import kotlinx.coroutines.tasks.await
 import org.junit.Assert.*
 
 import org.junit.Test
 
 class AuthenciationServiceTest {
 
-    @Test
-    fun writeNewUser() {
-    }
+
+    private val user= Users(userName ="niranjan",mail = "nloj@gmail.com",password ="3456")
 
     @Test
     fun sighnUpUser()
     {
-       GlobalScope.launch {
-
-       }
+        AuthenciationService.sighnUpUser(user)
+        val data = FirebaseUtils.database.child("users/${user.userId}").
+        get().result.to(Users::class.java)
+        assertEquals(data,user)
     }
 }
