f79a97f1901a2c96786ea6e83afd43090dc70da7
niranjannlc <niranjannlc10@gmail.com>
2022-01-01 19:32:17 +1200

    simple refactoring of chat onCreate

--- app/src/main/java/com/example/mechat/view/ChatDetailActivity.kt
+++ app/src/main/java/com/example/mechat/view/ChatDetailActivity.kt
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.util.Log.i
+import androidx.databinding.BindingAdapter
 import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.mechat.R
 import com.example.mechat.databinding.ActivityChatDetailBinding
 import com.example.mechat.databinding.ActivitySighUpBinding
 import com.example.mechat.utils.FirebaseUtils
+import com.example.mechat.view.adapter.DetailChatAdapter
 import com.example.mechat.viewmodal.AuthenciationViewModal
 import com.example.mechat.viewmodal.ChatDetailViewModal
 import com.example.mechat.viewmodal.ViewModalFactory
@@ -18,22 +21,30 @@ class ChatDetailActivity : AppCompatActivity()
 {
     private lateinit var binding: ActivityChatDetailBinding
     private lateinit var viewModal: ChatDetailViewModal
+    private lateinit var adapter: DetailChatAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_chat_detail)
         viewModal  = ViewModalFactory().create(ChatDetailViewModal ::class.java)
         binding.viewmodal = viewModal
-         var senderId = FirebaseUtils.firebaseUser?.uid
-         var recieverId =intent.getStringExtra("userId")
-         var userName = intent.getStringExtra("userName")
-        Log.i(" intent ", " $senderId was  $recieverId  thus $userName")
+        setSenderReceiver()
+        initializeRecyclerView()
+    }
+
+    private fun setSenderReceiver() {
+        var senderId = FirebaseUtils.firebaseUser?.uid
+        var recieverId = intent.getStringExtra("userId")
+        var userName = intent.getStringExtra("userName")
+        i(" intent ", " $senderId was  $recieverId  thus $userName")
         binding.username.text = userName
         binding.viewmodal.recieverId.value = recieverId
         binding.viewmodal.senderId.value = senderId
-        viewModal.setSenderReceiver( senderId, recieverId)
-//        binding.sendMessage.setOnClickListener{
-//            sendngMessage()
-//        } performed thorugh view modal ..............
+        viewModal.setSenderReceiver(senderId, recieverId)
+    }
+
+    private fun initializeRecyclerView() {
+        binding.chatMessageList.adapter = adapter
+        binding.chatMessageList.layoutManager = LinearLayoutManager(this)
     }
 
     private fun sendngMessage()
