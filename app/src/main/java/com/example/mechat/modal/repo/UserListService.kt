c9ab42fba9a11c9e421ec5ac03cf846f4346cc01
niranjannlc <niranjannlc10@gmail.com>
2022-01-02 19:20:21 +1200

    removing unnecessary codes by refactoring

--- app/src/main/java/com/example/mechat/modal/repo/UserListService.kt
+++ app/src/main/java/com/example/mechat/modal/repo/UserListService.kt
@@ -15,24 +15,7 @@ import kotlinx.coroutines.tasks.await
 object UserListService
 {
     var userList = MutableLiveData<List<Users>>()
-    fun getUserList()
-    {
-        val myTopPostsQuery = FirebaseUtils.database.child("users").orderByChild("userName")
-        myTopPostsQuery.addValueEventListener(object : ValueEventListener{
-            override fun onDataChange(snapshot: DataSnapshot)
-            {
-                val list = snapshot.children.map { it.getValue(Users::class.java)!! }
-                Log.d("TAG", "Value is: $list")
-                Log.i(" user list 00", snapshot.getValue().toString())
-                userList.value = list
-            }
 
-            override fun onCancelled(error: DatabaseError) {
-               Log.i(" error database ", error.details)
-            }
-
-        })
-    }
     suspend fun getUserListFromDb(): Any?
     {
         return try{
@@ -51,6 +34,5 @@ object UserListService
             Log.d("TAG", "Value is: $list")
             Log.i(" user list 00", list.toString())
             userList.value = list as List<Users>?
-
     }
 }
