4d323fbed58ed375a0559f3869927183e14c3452
niranjannlc <niranjannlc10@gmail.com>
2022-01-18 17:51:53 +0545

    splash activity could not be made better than this

--- app/src/main/java/com/example/mechat/view/SplashActivity.kt
+++ app/src/main/java/com/example/mechat/view/SplashActivity.kt
@@ -1,14 +1,18 @@
 package com.example.mechat.view
 
+import android.animation.ObjectAnimator
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.View
 import androidx.databinding.DataBindingUtil
 import androidx.transition.Fade
 import androidx.transition.TransitionManager
 import com.example.mechat.R
 import com.example.mechat.databinding.SplashScreenBinding
 import com.example.mechat.utils.FirebaseUtils
+import kotlinx.coroutines.delay
 
 class SplashActivity : AppCompatActivity()
 {
@@ -18,14 +22,20 @@ class SplashActivity : AppCompatActivity()
         super.onCreate(savedInstanceState)
         binnding = DataBindingUtil.setContentView(this, R.layout.splash_screen)
         animate()
+        Handler().postDelayed({
+            transferToNextActivity()
+        }, 3000)
         print(" $FirebaseUtils.firebaseUser  user in your town ...")
-        transferToNextActivity()
+
 
     }
 
     private fun animate() {
-        val mFade: Fade = Fade(Fade.IN)
+        val mFade: Fade = Fade(Fade.OUT)
         TransitionManager.beginDelayedTransition(binnding.splach, mFade)
+        val animator = ObjectAnimator.ofFloat(binnding.imageView7, View.ROTATION, -360f, 0f)
+        animator.duration = 1000
+        animator.start()
     }
 
     private fun transferToNextActivity() {
