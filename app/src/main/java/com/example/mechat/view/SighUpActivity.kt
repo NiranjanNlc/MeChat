ded28bf635c7fa5c26dce5d94307fd93f635deee
niranjannlc <niranjannlc10@gmail.com>
2022-01-28 12:33:28 +0545

    smple refractor

--- app/src/main/java/com/example/mechat/view/SighUpActivity.kt
+++ app/src/main/java/com/example/mechat/view/SighUpActivity.kt
@@ -7,14 +7,8 @@ import android.util.Log
 import androidx.databinding.DataBindingUtil
 import com.example.mechat.R
 import com.example.mechat.databinding.ActivitySighUpBinding
-import com.example.mechat.modal.data.Users
-import com.example.mechat.utils.Extensions.toast
-import com.example.mechat.utils.FirebaseUtils
-import com.example.mechat.utils.FirebaseUtils.firebaseAuth
 import com.example.mechat.viewmodal.AuthenciationViewModal
 import com.example.mechat.viewmodal.ViewModalFactory
-import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.ktx.Firebase
 
 class SighUpActivity : AppCompatActivity()
 {
@@ -23,8 +17,6 @@ class SighUpActivity : AppCompatActivity()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_sigh_up)
-      //  binding = ActivitySighUpBinding.inflate(layoutInflater)
-      //  setContentView(binding.root)
         viewModal  = ViewModalFactory().create(AuthenciationViewModal ::class.java)
         /*create a user*/
         binding.viewModal = viewModal
@@ -32,13 +24,6 @@ class SighUpActivity : AppCompatActivity()
             Log.d(" butttom of sighnup  ", " pressed")
             viewModal.sighnUp()
         }
-//        viewModal.processStatus.observe(this,{
-//           if (it)
-//           {
-//               Log.i("process ", "suveeded ")
-//               checksignUpStaus(it)
-//           }
-//        })
     }
 
     override fun onStart() {
@@ -46,26 +31,7 @@ class SighUpActivity : AppCompatActivity()
 
     }
 
-    private fun checksignUpStaus(procee:Boolean) {
+    private fun checksignUpStaus(procee:Boolean)
+    {
     }
-//        Log.i(" sucess authenciation" , viewModal.sighnUpStatus.value.toString())
-//        viewModal.sighnUpStatus.observe(this,{
-//            Log.d(" value " , " $it and $procee")
-//           if(it && procee) {
-//               toast("created account successfully under given condition !")
-//               FirebaseUtils.firebaseAuth.signOut()
-//               startActivity(Intent(this, Login::class.java))
-//               finish()
-//           }
-//            else if (it or procee)
-//           {
-//            //   checksignUpStaus(procee)
-//           //   toast("step 1")
-//           }
-//            else
-//           {
-//               toast(" eroor occurred ")
-//           }
-//           })
-//        }
 }
