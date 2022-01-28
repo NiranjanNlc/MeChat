477f5cfd721b08bacfc81cd495dc89b3bbeacb23
niranjannlc <niranjannlc10@gmail.com>
2022-01-28 14:07:12 +0545

    loading dialog during login

--- app/src/main/java/com/example/mechat/view/Login.kt
+++ app/src/main/java/com/example/mechat/view/Login.kt
@@ -3,6 +3,7 @@ package com.example.mechat.view
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import com.example.mechat.R
 import com.example.mechat.databinding.ActivityLoginBinding
 import com.example.mechat.utils.Extensions.toast
@@ -12,10 +13,13 @@ import com.google.firebase.auth.FirebaseUser
 
 class Login : AppCompatActivity() {
     private lateinit var binding : ActivityLoginBinding
+    lateinit var builder: AlertDialog.Builder
+    lateinit var dialog: AlertDialog
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        builder = AlertDialog.Builder(this)
         binding.button2.setOnClickListener {
             FirebaseUtils.firebaseAuth.signInWithEmailAndPassword(binding.username.text.toString(),
             binding.editTextTextPassword.text.toString())
@@ -44,5 +48,11 @@ class Login : AppCompatActivity() {
             toast("welcome back")
             finish()
         }
+    }
+    private fun loadingDialog(){
+        builder.setView(layoutInflater.inflate(R.layout.activity_loading_layout,null))
+        builder.setCancelable(false)
+        dialog = builder.create()
+        dialog.show()
     }
 }
