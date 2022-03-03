a9ce7dfeccee5db21db785b61ad8699944122650
niranjannlc <niranjannlc10@gmail.com>
2022-03-03 15:37:02 +0545

    add user detail actiity click listener desighn

--- app/src/main/java/com/example/mechat/view/activity/AddUserDetail.kt
+++ app/src/main/java/com/example/mechat/view/activity/AddUserDetail.kt
@@ -3,10 +3,34 @@ package com.example.mechat.view.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.mechat.R
+import com.example.mechat.databinding.ActivityAddUserDetailBinding
 
 class AddUserDetail : AppCompatActivity() {
+    private lateinit var binding: ActivityAddUserDetailBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_user_detail)
+        binding= ActivityAddUserDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.backArrow.setOnClickListener {
+            reverseBackToUserList()
+        }
+        binding.addImage.setOnClickListener {
+            updateProfileImage()
+        }
+        binding.saveInfo.setOnClickListener {
+            saveInfo()
+        }
+    }
+
+    private fun saveInfo() {
+        TODO("Not yet implemented")
+    }
+
+    private fun updateProfileImage() {
+        TODO("Not yet implemented")
+    }
+
+    private fun reverseBackToUserList() {
+        TODO("Not yet implemented")
     }
 }
