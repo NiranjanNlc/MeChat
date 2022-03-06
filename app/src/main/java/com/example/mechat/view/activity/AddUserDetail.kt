0b0c691f174c248b0d9fb615a0fdb428a41ad108
niranjannlc <niranjannlc10@gmail.com>
2022-03-06 12:47:25 +0545

    impllement reverse back button

--- app/src/main/java/com/example/mechat/view/activity/AddUserDetail.kt
+++ app/src/main/java/com/example/mechat/view/activity/AddUserDetail.kt
@@ -1,16 +1,20 @@
 package com.example.mechat.view.activity
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.mechat.R
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.mechat.databinding.ActivityAddUserDetailBinding
+import com.example.mechat.utils.Extensions.toast
+import com.example.mechat.utils.FirebaseUtils
+import java.io.File
 
 class AddUserDetail : AppCompatActivity() {
     private lateinit var binding: ActivityAddUserDetailBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding= ActivityAddUserDetailBinding.inflate(layoutInflater)
+        binding = ActivityAddUserDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.backArrow.setOnClickListener {
             reverseBackToUserList()
@@ -31,10 +35,41 @@ class AddUserDetail : AppCompatActivity() {
         val intent = Intent()
         intent.action = Intent.ACTION_GET_CONTENT
         intent.type = "image/*"
-
+        previewRequest.launch(intent)
     }
 
     private fun reverseBackToUserList() {
-        TODO("Not yet implemented")
+        startActivity(Intent(this, WelecomActivity::class.java))
+        toast("Logged out sucess fully ")
+        finish()
+    }
+
+    val previewRequest =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            if (it.resultCode == RESULT_OK) {
+                val list = it.data
+                 binding.profilePic.setImageURI(list?.data)
+                 sendDataToFireBaseStorage(list)
+            }
+
+        }
+
+    private fun sendDataToFireBaseStorage(data: Intent?)
+    {
+        var file = data?.toUri(0)
+        val riversRef = FirebaseUtils.storage.reference.child("images/${file.toString()}")
+        val uploadTask = data?.data?.let { riversRef.putFile(it) }
+
+// Register observers to listen for when the download is done or if it fails
+        if (uploadTask != null) {
+            uploadTask.addOnFailureListener {
+                // Handle unsuccessful uploads
+            }.addOnSuccessListener { taskSnapshot ->
+                // taskSnapshot.metadata contains file metadata such as size, content-type, etc.
+                // ...
+
+            }
+        }
+
     }
 }
