4759522bb294d53a0f5e4249a7fef4c372430889
niranjannlc <niranjannlc10@gmail.com>
2022-03-07 15:01:25 +0545

    downlaod url obtained for images

--- app/src/main/java/com/example/mechat/view/activity/AddUserDetail.kt
+++ app/src/main/java/com/example/mechat/view/activity/AddUserDetail.kt
@@ -1,17 +1,21 @@
 package com.example.mechat.view.activity
 
 import android.content.Intent
-import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.result.contract.ActivityResultContracts
 import com.example.mechat.databinding.ActivityAddUserDetailBinding
 import com.example.mechat.utils.Extensions.toast
 import com.example.mechat.utils.FirebaseUtils
-import java.io.File
+import com.google.android.gms.tasks.OnSuccessListener
+import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.UploadTask
 
 class AddUserDetail : AppCompatActivity() {
     private lateinit var binding: ActivityAddUserDetailBinding
+    private lateinit var riversRef: StorageReference
+    private lateinit var profileImageUrl: String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAddUserDetailBinding.inflate(layoutInflater)
@@ -28,7 +32,7 @@ class AddUserDetail : AppCompatActivity() {
     }
 
     private fun saveInfo() {
-        TODO("Not yet implemented")
+         println(" Now implemented ")
     }
 
     private fun updateProfileImage() {
@@ -48,7 +52,6 @@ class AddUserDetail : AppCompatActivity() {
         registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
             if (it.resultCode == RESULT_OK) {
                 val list = it.data
-                 binding.profilePic.setImageURI(list?.data)
                  sendDataToFireBaseStorage(list)
             }
         }
@@ -56,21 +59,27 @@ class AddUserDetail : AppCompatActivity() {
     private fun sendDataToFireBaseStorage(data: Intent?)
     {
         var file = data?.toUri(0)
-        val riversRef = FirebaseUtils.storage.reference.child("images/${file.toString()}")
+        riversRef = FirebaseUtils.storage.reference.child("images/${
+            FirebaseUtils.firebaseUser?.uid}")
         val uploadTask = data?.data?.let { riversRef.putFile(it) }
 
 // Register observers to listen for when the download is done or if it fails
         if (uploadTask != null) {
             uploadTask.addOnFailureListener {
-                println(" success story ")
+                Log.d(" success story ", it.message.toString())
+                // binding.profilePic.setImageURI(list?.data)
                 // Handle unsuccessful uploads
-            }.addOnSuccessListener { taskSnapshot ->
-                // taskSnapshot.metadata contains file metadata such as size, content-type, etc.
-                // ...
-                println(" Unsucesss story ")
-
+            }.addOnSuccessListener ( OnSuccessListener<UploadTask.TaskSnapshot> { taskSnapshot ->
+                taskSnapshot.storage.downloadUrl.addOnSuccessListener {
+                    Log.d(" Unsucesss story "," haha ")
+                    profileImageUrl = it.toString()
+                    loadProfilePic(profileImageUrl)
+                }
+            })
             }
         }
-
+    private fun loadProfilePic(riversRef: String) {
+         println(" Will be implemented thrpough picaso")
+        Log.d(" url download ", profileImageUrl)
     }
 }
