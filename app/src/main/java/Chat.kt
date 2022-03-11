0fa538a2533e1489974c82139579489a308f5f56
niranjannlc <niranjannlc10@gmail.com>
2022-03-12 01:13:32 +1300

    refactor

--- app/src/main/java/Chat.kt
+++
@@ -1,3 +0,0 @@
-data class Chat(
-    val uQI50mNe3WPJ1ueq8pyz3UzWv9D2: UQI50mNe3WPJ1ueq8pyz3UzWv9D2
-)
--- app/src/main/java/UQI50mNe3WPJ1ueq8pyz3UzWv9D2.kt
+++
@@ -1,7 +0,0 @@
-data class UQI50mNe3WPJ1ueq8pyz3UzWv9D2(
-    val key: String,
-    val receiverId: String,
-    val senderId: String,
-    val text: String,
-    val timeStamp: String
-)
